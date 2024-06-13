b1535e8fbc27f11c5a2e9be76bbf5994856555cf
Mohammed Ly <lybhoye@yahoo.com>
2024-06-13 17:41:18 +0200

    changes made today

--- GitTestProject/src/testMe/MyNewTestClass.java
+++ GitTestProject/src/testMe/MyNewTestClass.java
@@ -5,6 +5,8 @@ public class MyNewTestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Bismillah!!!");
+System.out.println("Yaa man laa ya muut!!!");
+System.out.println("ALhamdulillah !!!!");
 	}
 
 }
