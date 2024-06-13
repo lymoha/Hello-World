dfbdd5e98a5e315ea3da761aaaee212507a024ab
Mohammed Ly <lybhoye@yahoo.com>
2024-06-13 18:04:47 +0200

    New changes have been made!

---
+++ GitTestProject/src/testMe/ANewClasToBeTested.java
@@ -0,0 +1,8 @@
+package testMe;
+
+public class ANewClasToBeTested {
+
+	public double sqrtOfANumber(double num) {
+		return Math.sqrt(num);
+	}
+}
--- GitTestProject/src/testMe/MyNewTestClass.java
+++ GitTestProject/src/testMe/MyNewTestClass.java
@@ -3,11 +3,18 @@ package testMe;
 public class MyNewTestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Bismillah!!!");
-System.out.println("Yaa man laa ya muut!!!");
-System.out.println("ALhamdulillah !!!!");
-System.out.println("Yo dyomirawo on wallitolan!!!");
+		ANewClasToBeTested sqrt = new ANewClasToBeTested();
+		
+		double result = sqrt.sqrtOfANumber(77);
+		
+	
+		
+		System.out.println("Bismillah!!!");
+		System.out.println("Yaa man laa ya muut!!!");
+		System.out.println("ALhamdulillah !!!!");
+		System.out.println("Yo dyomirawo on wallitolan!!!");
+
+		System.out.println(Math.round(result));
 	}
 
 }
